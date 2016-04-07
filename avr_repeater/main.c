1effedb72ff9336980e02eebdf1d5d51543b8089
Ben Oxley <ben.oxley@gmail.com>
2016-04-08 00:48:19 +0100

    Initial commit of dht-22 sensor

--- avr_repeater/main.c
+++ avr_repeater/main.c
@@ -1,7 +1,11 @@
 /**
- * UKHASnet Dumb-Sensor Code by Phil Crump M0DNY
- * Bare metal AVR version by Jon Sowman M0JSN <jon@jonsowman.com>
+ * UKHASnet Repeater Code by Phil Crump M0DNY
+ * Bare metal AVR version by Jon Sowman M0JSN <jon+github@jonsowman.com>
  * Based on UKHASnet rf69_repeater by James Coxon M6JCX
+ *
+ * Uses the ukhasnet-rfm69 library. Please see README.me in the git repository
+ * for instructions to get and use the library.
+ * https://github.com/UKHASnet/ukhasnet-rfm69
  */
 
 #include <avr/io.h>
@@ -13,37 +17,317 @@
 #include <stdlib.h>
 #include <util/delay.h>
 
-#include "RFM69Config.h"
-#include "RFM69.h"
+#include "ukhasnet-rfm69.h"
+#include "dht22.h"
 #include "nodeconfig.h"
+
+/* Zombie mode */
+typedef enum zombie_mode_t {MODE_NORMAL, MODE_ZOMBIE} zombie_mode_t;
+
+/* Global variables local to this compilation unit */
+static float battV = 0.0;
+static uint32_t count = 1, data_interval = 2;
+static char sequence_id = 'a';
+static char packet_buf[64];
+static zombie_mode_t zombie_mode = MODE_NORMAL;
+// static struct dht22 dhtSensor;
 
 /* Private prototypes */
 float get_batt_voltage(void);
-int16_t gen_data(char *buf);
+int16_t gen_data(char* buf);
 void init(void);
-
-/* Global variables local to this compilation unit */
-static int16_t packet_len;
-static float battV = 0.0;
-static uint32_t count = 1, data_interval = 2;
-static uint8_t sequence_id = 97; // 'a'
-static char databuf[64];
-static uint8_t buf[64], len;
-static uint8_t zombie_mode = 0; // Stores current status: 0 - Full Repeating, 1 - Low Power shutdown, (beacon only)
-/********************************************************************************
-Function Prototypes
-********************************************************************************/
-void USART_init(void);
-void USART_send( unsigned char data);
-void USART_putstring(char* StringPtr);
-uint16_t getRandBetween(uint16_t lower, uint16_t upper);
-void enableRepeat(void);
-int16_t gen_data(char *buf);
+uint16_t getRandBetween(const uint16_t lower, const uint16_t upper);
+void zombieMode(void);
 void loop(void);
 void sendPacket(void);
-void sleep(uint8_t);
+
 /**
- * Measure the battery voltage.
+ * Initialise internal and external peripherals, including reading config and
+ * setting power savings modes.
+ */
+void init(void)
+{
+    /* Turn off peripherals that we don't use */
+    PRR |= _BV(PRTWI) | _BV(PRTIM2) | _BV(PRTIM1) | _BV(PRTIM0) | _BV(PRUSART0);
+
+    while(rf69_init() != RFM_OK)
+        _delay_ms(100);
+
+    // dht_init(&dhtSensor,PB1);
+    /* Transmit an initial packet */
+    sendPacket();
+}
+
+/**
+ * A wrapper function to generate a packet using gen_data() and then transmit
+ * it using the RFM69 radio.
+ */
+void sendPacket(void)
+{
+    uint16_t packet_len;
+
+    if(sequence_id > 'z')
+    {
+        sequence_id = 'b';
+    }
+    packet_len = gen_data(packet_buf);
+    rf69_send((rfm_reg_t *)packet_buf, packet_len, RFM_POWER);
+    sequence_id++;
+}
+
+/**
+ * If ENABLE_ZOMBIE_MODE is defined in nodeconfig.h, this function should be
+ * called regularly to bring the node in and out of zombie mode as a function
+ * of battery voltage. ZOMBIE_THRESHOLD is the zombie mode threshold battery
+ * voltage in Volts, define this also in nodeconfig.h.
+ */
+void zombieMode(void)
+{
+#ifdef ENABLE_ZOMBIE_MODE
+    if(battV > (ZOMBIE_THRESHOLD + ZOMBIE_HYST) && zombie_mode == MODE_ZOMBIE)
+    {
+        rf69_set_mode(RFM69_MODE_RX);
+        zombie_mode = MODE_NORMAL;
+#ifdef SENSITIVE_RX
+        rf69_set_lna_mode(RF_TESTLNA_SENSITIVE);
+#endif /* SENSITIVE_RX */
+    }
+    else if(battV < ZOMBIE_THRESHOLD && zombie_mode == MODE_NORMAL)
+    {
+        rf69_set_mode(RFM69_MODE_SLEEP);
+        zombie_mode = MODE_ZOMBIE;
+    }
+#else
+    rf69_set_mode(RFM69_MODE_RX);
+    zombie_mode = MODE_NORMAL;
+#ifdef SENSITIVE_RX
+    rf69_set_lna_mode(RF_TESTLNA_SENSITIVE);
+#endif /* SENSITIVE_RX */
+#endif /* ENABLE_ZOMBIE_MODE */
+}
+
+/**
+ * Call init functions and then run forever.
+ */
+int main(void)
+{
+    init();
+    rf69_set_mode(RFM69_MODE_SLEEP);
+
+    /* Initial data interval = BEACON_INTERVAL since count = 0 */
+    data_interval = BEACON_INTERVAL/8;
+
+    while(1)
+        loop();
+
+    return 0;
+}
+
+/**
+ * Contents of this function are called every iteration of the infinite main
+ * loop. We should do houskeeping, then transmit/receive as and when it is
+ * necessary.
+ */
+void loop(void)
+{
+    bool ispacket;
+    uint8_t i, j, k, start_bracket, end_bracket;
+    uint16_t delaytime, packet_len;
+    int16_t lastrssi;
+    rfm_reg_t len;
+
+    count++;
+    wdt_reset();
+
+    if( zombie_mode == MODE_NORMAL )
+    {
+        rf69_set_mode(RFM69_MODE_RX);
+
+        /* This loop is calculated to take roughly 8 seconds, i.e. 255
+         * iterations with a 30ms delay */
+        for(i = 0; i < 255; i++) {
+            wdt_reset();
+            _delay_ms(30);
+            rf69_receive((rfm_reg_t *)packet_buf, &len, &lastrssi, &ispacket);
+
+            /* The boolean variable 'ispacket' will be true if rf69_receive
+             * tells us there is a packet waiting in the RFM69 receive buffer
+             */
+            if( ispacket )
+            {
+                /* Find end of packet & start of repeaters */
+                end_bracket = 255;
+                start_bracket = 255;
+                /* TODO: This should be replaced with strstr() */
+                for (k = 0; k < len; k++)
+                {
+                    if (packet_buf[k] == '[')
+                    {
+                        start_bracket = k;
+                    }
+                    else if (packet_buf[k] == ']')
+                    {
+                        end_bracket = k;
+                        packet_buf[k+1] = '\0';
+                        break;
+                    }
+                }
+
+                /* Need to take the recieved buffer and decode it and 
+                 * add a reference, under 3 conditions:
+                 * 1) The number of 'hops' remaining is > 0
+                 * 2) We found an end bracket in the packet, i.e. it is not
+                 * malformed in some way
+                 * 3) We have not /already/ repeated this packet
+                 * */
+                if (packet_buf[0] > '0' 
+                        && end_bracket != 255
+                        && strstr((const char *)&packet_buf[start_bracket], NODE_ID) == NULL)
+                {
+                    /* Reduce the TTL (hops) */
+                    packet_buf[0]--;
+
+                    /* Add the repeater ID */
+                    packet_len = end_bracket + sprintf((char *)&packet_buf[end_bracket], ",%s]", NODE_ID);
+
+                    /* Random delay to try and avoid packet collision */
+                    delaytime = getRandBetween(5u, 80u);
+                    for(j = 0; j < delaytime; j++)
+                        _delay_ms(10);
+
+                    /* Now send the repeated packet */
+                    rf69_send((rfm_reg_t *)packet_buf, packet_len, RFM_POWER);
+                }
+            }
+        }
+    }
+    else
+    {
+        /* Battery Voltage Low - Zombie Mode, ignore all repeating
+         * functionality of the node 
+         * Low Power Sleep for 8 seconds
+         */
+        rf69_set_mode(RFM69_MODE_SLEEP);
+
+        /* Enable the watchdog and sleep for 8 seconds */
+        wdt_enable(WDTO_8S);
+        WDTCSR |= (1 << WDIE);
+        set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+        sleep_enable();
+        sei();
+        sleep_cpu();
+        sleep_disable();
+    }
+
+    /* Time to send a beacon? */
+    if(count >= data_interval)
+    {
+        /* Send a packet */
+        sendPacket();
+
+        /* When will we send the next beacon? */
+        data_interval = getRandBetween((BEACON_INTERVAL/8), 
+                (BEACON_INTERVAL/8)+2) + count;
+
+        /* Check if we need to enter or leave zombie mode */
+        zombieMode();
+    }
+}
+
+/**
+ * Given a char buffer, generate a UKHASnet packet and return the length of
+ * the packet written to the supplied buffer.
+ * @param buf The char buffer into which to write the packet
+ * @returns The length of the packet that was written
+ */
+int16_t gen_data(char *buf)
+{
+    int8_t temp = 0;
+    float tempdht = -1.0;
+    float humdht = -1.0;
+    static char tempstr[14];
+    static char humstr[14];
+#ifdef LOCATION_STRING
+    if(sequence_id=='a' || sequence_id=='z') {
+        sprintf(buf, "%u%cL%s", NUM_REPEATS, sequence_id, LOCATION_STRING);
+    } else {
+        sprintf(buf, "%u%c", NUM_REPEATS, sequence_id);
+    }
+#else
+    sprintf(buf, "%u%c", NUM_REPEATS, sequence_id);
+#endif
+
+    /* Read the temperature and if successful, add to packet */
+    if( rf69_read_temp(&temp) == RFM_OK ) {
+        sprintf(buf, "%sT%i.0", buf, temp);
+    }
+    //Make PB0 an output 
+    DDRB |= (1 << PB0);
+    PORTB |= (1 << PB0);
+
+    for (int i = 0; i < 6; i++){
+        int res = dht22_read(&tempdht, &humdht);
+        // if (res==DHT_ERROR_ACK_TOO_LONG){
+        //     sprintf(buf,"%sACK",buf);
+        // }
+        // if (res==DHT_ERROR_NOT_PRESENT){
+        //     sprintf(buf,"%sNPRES",buf);
+        // }
+        // if (res==DHT_ERROR_SYNC_TIMEOUT){
+        //     sprintf(buf,"%sESYNC",buf);
+        // }
+        // if (res==DHT_ERROR_DATA_TIMEOUT){
+        //     sprintf(buf,"%sEDATA",buf);
+        // }
+        // if (res==DHT_ERROR_NONE){
+        //     sprintf(buf,"%sGOOD",buf);
+        // }
+        // if (res==DHT_ERROR_CHECKSUM){
+        //     sprintf(buf,"%sCSERR",buf);
+        // }
+
+        if (res==0){
+            char* tempStrPos;
+            char* humStrPos;
+            tempStrPos = dtostrf(tempdht,7, 2, tempstr);
+            while( *tempStrPos == ' ' )
+                tempStrPos++;
+            sprintf(buf, "%sT%sH%d", buf, tempStrPos,(int)humdht);
+            break;
+        }
+    }
+    // dht_read_data(&dhtSensor, &tempdht, &humdht);
+    // dht_read_data(&dhtSensor, &tempdht, &humdht);
+    // dht_read_data(&dhtSensor, &tempdht, &humdht);
+    // dht_read_data(&dhtSensor, &tempdht, &humdht);
+    // dht_read_data(&dhtSensor, &tempdht, &humdht);
+    // dht_read_data(&dhtSensor, &tempdht, &humdht);
+
+
+    /* Battery Voltage */
+#if ENABLE_BATTV_SENSOR == 1
+    battV = get_batt_voltage();
+    char* battStr;
+    char tempStrB[14]; /* make buffer large enough for 7 digits */
+    battStr = dtostrf(battV,7,2,tempStrB);
+    /* Remove leading spaces by incrementing the pointer */
+    while( *battStr == ' ' )
+        battStr++;
+    sprintf(buf, "%sV%s", buf, battStr);
+#endif
+
+    /* If zombie mode is enabled, put the current zombie status in telem */
+#ifdef ENABLE_ZOMBIE_MODE
+    sprintf(buf, "%sZ%d", buf, zombie_mode);
+#endif /* ENABLE_ZOMBIE_MODE */
+
+    return sprintf(buf, "%s[%s]", buf, NODE_ID);
+}
+
+/**
+ * Measure the battery voltage as measured by a potential divider on BATTV_PIN
+ * defined in nodeconfig.h. BATTV_SCALEFACTOR should also be set appropriately
+ * in this file.
  * @returns The voltage of the battery in Volts
  */
 float get_batt_voltage(void)
@@ -77,271 +361,24 @@ float get_batt_voltage(void)
     return (float)(res * 0.001074219 * BATTV_SCALEFACTOR * BATTV_FUDGE);
 }
 
-int16_t gen_data(char *buf)
+/**
+ * Get a random number between an upper and lower limit. The random number
+ * generation itself is rand(), which must be supported in the platform of 
+ * your choice.
+ * @param lower The lower boundary
+ * @param upper The upper boundary
+ * @returns The resulting random number between lower and upper.
+ */
+uint16_t getRandBetween(const uint16_t lower, const uint16_t upper)
 {
-    int8_t temp = 0;
-
-#ifdef LOCATION_STRING
-    if(sequence_id=='a' || sequence_id=='z') {
-        sprintf(buf, "%u%cL%s", NUM_REPEATS, sequence_id, LOCATION_STRING);
-    } else {
-        sprintf(buf, "%u%c", NUM_REPEATS, sequence_id);
-    }
-#else
-    sprintf(buf, "%u%c", NUM_REPEATS, sequence_id);
-#endif
-
-    temp = rf69_readTemp();
-    sprintf(buf, "%sT%i.0", buf, temp);
-
-    // Battery Voltage
-#if ENABLE_BATTV_SENSOR == 1
-    battV = get_batt_voltage();
-    char* battStr;
-    char tempStrB[14]; //make buffer large enough for 7 digits
-    battStr = dtostrf(battV,7,2,tempStrB);
-    while( (strlen(battStr) > 0) && (battStr[0] == 32) )
-    {
-        strcpy(battStr,&battStr[1]);
-    }
-    sprintf(buf, "%sV%s", buf, battStr);
-#endif
-
-    return sprintf(buf, "%s[%s]", buf, NODE_ID);
-}
-
-void init(void)
-{
-
-    /* Turn off peripherals that we don't use */
-    PRR |= _BV(PRTWI) | _BV(PRTIM2) | _BV(PRTIM1) | _BV(PRTIM0) ;//| _BV(PRUSART0);
-
-
-
-    while(!rf69_init())
-        _delay_ms(100);
-    sendPacket();
-
-    enableRepeat();
-
-}
-
-void sendPacket(void){
-    if(sequence_id > 122){
-      sequence_id = 98; //'b'
-    }
-    packet_len = gen_data(databuf);
-    rf69_send((uint8_t*)databuf, packet_len, RFM_POWER);
-    sequence_id++;
-    wdt_reset();
-    _delay_ms(1000);
-    wdt_reset();
-}
-
-void enableRepeat(void){
-    
-#ifdef ENABLE_ZOMBIE_MODE
-   if(battV > ZOMBIE_THRESHOLD) {
-     rf69_setMode(RFM69_MODE_RX);
-     zombie_mode=0;
-     #ifdef SENSITIVE_RX
-      rf69_SetLnaMode(RF_TESTLNA_SENSITIVE);
-     #endif
-   } else {
-     rf69_setMode(RFM69_MODE_SLEEP);
-     zombie_mode=1;
-   }
-  #else
-   rf69_setMode(RFM69_MODE_RX);
-   zombie_mode=0;
-   #ifdef SENSITIVE_RX
-    rf69_SetLnaMode(RF_TESTLNA_SENSITIVE);
-   #endif
-  #endif
-}
-
-int main(void)
-{
-    init();
-    rf69_setMode(RFM69_MODE_SLEEP);
-    #ifdef ENABLE_UART_OUTPUT
-    USART_init();
-    #endif
-    /* Initial data interval = BEACON_INTERVAL since count = 0 */
-    data_interval = BEACON_INTERVAL;
-
-    while(1)
-    {
-        loop();
-    }
-
-    return 0;
-}
-
-void loop(void)
-{
-  count++;
-  wdt_reset();
-  if(zombie_mode==0) {
-    rf69_setMode(RFM69_MODE_RX);
-    for(uint8_t i=0; i<255; i++) {
-      wdt_reset();
-      //LowPower.idle(SLEEP_30MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_ON, TWI_OFF);
-      _delay_ms(30);
-      if (rf69_checkRx()) {
-        rf69_recv(buf, &len);
-        
-        #ifdef ENABLE_UART_OUTPUT
-         uint8_t rx_rssi = rf69_lastRssi();
-         for (int j=0; j<len; j++) {
-             USART_send((char)buf[j]);
-             if(buf[j]==']') break;
-         }
-         USART_send('|');
-         char str[5];
-         sprintf((char *)&str[0],"%i",rx_rssi);
-         USART_putstring(str);
-        #endif
-
-        // find end of packet & start of repeaters
-        uint8_t end_bracket = -1, start_bracket = -1;        
-        for (int k=0; k<len; k++) {
-          if (buf[k] == '[') {
-            start_bracket = k;
-          }
-          else if (buf[k] == ']') {
-            end_bracket = k;
-            buf[k+1] = '\0';
-            break;
-          }
-        }
-
-        // Need to take the recieved buffer and decode it and add a reference 
-        if (buf[0] > '0' && end_bracket != -1 && strstr((const char *)&buf[start_bracket], NODE_ID) == NULL) {
-          // Reduce the repeat value
-          buf[0]--;
-          
-          // Add the repeater ID
-          packet_len = end_bracket + sprintf((char *)&buf[end_bracket], ",%s]", NODE_ID);
-          uint16_t delayValue = getRandBetween(5u, 80u);
-          for (int j = 0; j < delayValue;j++){
-          //random delay to try and avoid packet collision
-              _delay_ms(10);
-          }
-          
-          rf69_send((uint8_t*)buf, packet_len, RFM_POWER);
-          #ifdef ENABLE_UART_OUTPUT
-             for (int j=0; j<packet_len; j++) {
-                 if(buf[j]==']'){
-                    USART_send((char)buf[j]);
-                    USART_send('\n');
-                    break;
-                 }
-                 USART_send((char)buf[j]);
-             }
-            #endif
-        }
-      }
-    }
-  } else {
-    // Battery Voltage Low - Zombie Mode
-    
-    // Low Power Sleep for 8 seconds
-    rf69_setMode(RFM69_MODE_SLEEP);
-    sleep(8u);
-  }
-  
-  if (count >= data_interval){
-    sequence_id++;
-
-    if(sequence_id > 122){
-      sequence_id = 98; //'b'
-    }
-    
-    packet_len = gen_data(databuf);
-    rf69_send((uint8_t*)databuf, packet_len, RFM_POWER);
-    #ifdef ENABLE_UART_OUTPUT
-     // Print own Beacon Packet
-     for (int j=0; j<packet_len; j++)
-     {
-         if(databuf[j]==']') // Check for last char in packet
-         {
-             USART_send(databuf[j]);
-             USART_send('\n');
-             break;
-         }
-         USART_send(databuf[j]);
-     }
-    #endif
-    
-    data_interval = getRandBetween((BEACON_INTERVAL/8), (BEACON_INTERVAL/8)+2) + count;
-    #ifdef ENABLE_ZOMBIE_MODE
-     if(battV > ZOMBIE_THRESHOLD && zombie_mode==1) {
-         rf69_setMode(RFM69_MODE_RX);
-         zombie_mode=0;
-         #ifdef SENSITIVE_RX
-          rf69_SetLnaMode(RF_TESTLNA_SENSITIVE);
-         #endif
-     } else if (battV < ZOMBIE_THRESHOLD && zombie_mode==0) {
-         rf69_setMode(RFM69_MODE_SLEEP);
-         zombie_mode=1;
-     }
-    #endif
-  }
-}
-
-uint16_t getRandBetween(uint16_t lower, uint16_t upper){
     return (uint16_t)(((double)rand() / ((double)RAND_MAX + 1) * (upper-lower))+lower);
 }
 
-/********************************************************************************
-usart Related
-********************************************************************************/
-void USART_init(void){
- 
- UBRR0H = (uint8_t)(BAUD_PRESCALLER>>8);
- UBRR0L = (uint8_t)(BAUD_PRESCALLER);
- UCSR0B = (1<<RXEN0)|(1<<TXEN0);
- UCSR0C = (3<<UCSZ00);
-}
-
-void USART_send( unsigned char data){
- 
- while(!(UCSR0A & (1<<UDRE0)));
- UDR0 = data;
- 
-}
- 
-void USART_putstring(char* StringPtr){
- 
-while(*StringPtr != 0x00){
- USART_send(*StringPtr);
- StringPtr++;}
- 
-}
-
-void sleep(uint8_t seconds){
-   /* Enable the watchdog and sleep for 8 seconds */
-    for (int i = 0; i < seconds; i++){
-        wdt_enable(WDTO_1S);
-        WDTCSR |= (1 << WDIE);
-        set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-        sleep_enable();
-        sei();
-        sleep_cpu();
-        sleep_disable();
-        wdt_reset();
-    }
-    wdt_enable(WDTO_8S);
-    wdt_reset();
-}
-
 /**
- * Watchdog interrupt
+ * Watchdog interrupt, we have just woken from sleep. Kill the watchdog and
+ * then exit the ISR, main execution continues.
  */
 ISR(WDT_vect)
 {
     wdt_disable();
 }
-
-
